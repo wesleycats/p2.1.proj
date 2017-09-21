74167454553c0beb97289c39522b48df327f09e7
Fahdon <thejes@hotmail.nl>
2017-09-21 09:40:21 +0200

    Shapes movement v1.1
    Shapes
    
    Added:
    
    - updated the movement of the player

--- Game/Shapes/Assets/Scripts/Player/Player_Movement.cs
+++ Game/Shapes/Assets/Scripts/Player/Player_Movement.cs
@@ -6,29 +6,67 @@ public class Player_Movement : MonoBehaviour {
 
 	float _PlayerCurrentX;
 	float _PlayerCurrentY;
-	float _PlayerXUpdated = 0;
+	float _PlayerYUpdated = 0;
 	float _Gravity = 0.999f;
+	float _JumpingForce = 0;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		_PlayerCurrentX = gameObject.transform.position.x;
 		_PlayerCurrentY = gameObject.transform.position.y;
 
-		if (Input.GetButtonDown("Jump"))
+		if (Input.GetKeyDown("w"))
 		{
-			_PlayerXUpdated = 0.1f;
+			
+			StartCoroutine("Jumping");
+
 		}
 
-		_PlayerXUpdated-= _Gravity;
+		if (Input.GetKeyDown("s"))
+		{
+
+			StartCoroutine("Dropping");
+
+		}
 
 
 
+		
 
-		gameObject.transform.position = new Vector3(_PlayerCurrentX,_PlayerCurrentY + _PlayerXUpdated);
+		
+		gameObject.transform.position = new Vector3(_PlayerCurrentX,_PlayerCurrentY + _JumpingForce);
+	}
+
+	// Jumping handels the jumping of the character
+	private IEnumerator Jumping()
+	{
+		//print("hai");
+		for (float i = 0; i < 15; i++)
+		{
+			
+			_JumpingForce = i / 15;
+			
+			yield return null;
+		}
+		_JumpingForce = 0;
+	}
+	
+	// Dropping handels the falling down of the character
+	private IEnumerator Dropping()
+	{
+		//print("hai");
+		for (float i = 0; i < 15; i++)
+		{
+			//print("hai");
+			_JumpingForce = - i / 15;
+
+			yield return null;
+		}
+		_JumpingForce = 0;
 	}
 }
