9fedb50e8ef009c0ce516faa7cc2e98cbdb352f4
Fahdon <thejes@hotmail.nl>
2017-09-25 14:45:55 +0200

    Merge remote-tracking branch 'refs/remotes/origin/Wesley' into Jessey
    # Conflicts:
    #	Documentation/Game Design Document.docx
    #	Game/Shapes/Assets/Scenes/Main.unity
    #	Game/Shapes/Library/CurrentLayout.dwlt
    #	Game/Shapes/Library/CurrentMaximizeLayout.dwlt
    #	Game/Shapes/Library/InspectorExpandedItems.asset
    #	Game/Shapes/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Game/Shapes/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	Game/Shapes/Library/assetDatabase3
    #	Game/Shapes/Library/expandedItems
    #	Game/Shapes/Library/metadata/dc/dcc0015df346e8740857c169e9c1cec1

---
+++ Game/Shapes/Assets/Scripts/Background/BGMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMovement : MonoBehaviour {
+
+    private float _BGCurrentX;
+    private float _BackgroundX;
+    [SerializeField]
+    private float _MapSpeed;
+    [SerializeField]
+    private GameObject _Background;
+
+    // Use this for initialization
+    void Start () { 
+        _BackgroundX = 25.1f;
+        //_MapSpeed = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckPosition();
+        _BGCurrentX = this.transform.position.x;
+
+        transform.position += new Vector3(-_MapSpeed * Time.fixedDeltaTime, 0.0f, 0.0f);
+	}
+
+    void CheckPosition()
+    {
+        if (_BGCurrentX <= -_BackgroundX)
+        {
+            //Debug.Log("BG");
+            //Instantiate(_Background, new Vector3(_BackgroundX, transform.position.y), transform.rotation);
+            //Destroy(gameObject);
+
+            transform.position = new Vector3(_BackgroundX, transform.position.y);
+        }
+    }
+}
